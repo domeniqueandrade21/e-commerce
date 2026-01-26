d0bf5600932602159ee136ab25d5d7ef31ebcd02
Dom <domeniqueandrade21@gmail.com>
2026-01-26 11:46:12 -0500

    Botones añadidos

--- models/producto.js
+++ models/producto.js
@@ -1,12 +1,14 @@
-const mongoose = requiere ('mongoose');
+const mongoose = require('mongoose');
 
-//Definir el esquema
 const productoSchema = new mongoose.Schema({
-    nombre: {type: String, requiere: true},
-    precio: {type: Number, requiere: true},
-    categoria: {type: String, requiere: true},
-    imagen: {type: String, default: "https://dummyimage.com/200x200/000/fff"},
+    nombre: { type: String, required: true },
+    precio: { type: Number, required: true },
+    categoria: { type: String, required: true },
+    descripcion: { type: String, required: true }, // NUEVO
+    imagen: {
+        type: String,
+        default: 'https://dummyimage.com/200x200/000/fff'
+    }
 });
 
-//Exportar el modelo para usar
 module.exports = mongoose.model('Producto', productoSchema);
